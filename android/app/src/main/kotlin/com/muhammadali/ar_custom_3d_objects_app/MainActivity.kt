1a4be0af9aaa22218e42f8297fe479d715e276aa
asnchandu495 <chandu.allam495@gmail.com>
2023-10-11 18:33:29 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/muhammadali/ar_custom_3d_objects_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.muhammadali.ar_custom_3d_objects_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ar_custom_3d_objects_app/custom_3d_objects_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Custom3dObjectScreen(),
+    );
+  }
+}
+
+
